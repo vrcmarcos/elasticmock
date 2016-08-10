75b79b7f6a640d3c854a3c9d186bcb64c1321aa0
Marcos Cardoso <vrcmarcos@gmail.com>
2016-08-10 14:04:37 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ElasticMock',
-    version='0.0.1',
+    version='1.0.0',
     url='https://github.com/elasticmock',
     license='MIT',
     author='Marcos Cardoso',
