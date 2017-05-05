6761d8230d59031ad5183615f68a71e51f5f0309
Matthew Breeden <breeden.matt@gmail.com>
2017-05-04 17:05:46 -0700

    Allow ignored params to Elasticsearch

--- elasticmock/__init__.py
+++ elasticmock/__init__.py
@@ -9,7 +9,7 @@ from elasticmock.fake_elasticsearch import FakeElasticsearch
 ELASTIC_INSTANCES = {}
 
 
-def _get_elasticmock(hosts=None):
+def _get_elasticmock(hosts=None, *args, **kwargs):
     elastic_key = 'localhost:9200' if hosts is None else '{0}:{1}'.format(hosts[0].get('host'), hosts[0].get('port'))
     if elastic_key in ELASTIC_INSTANCES:
         connection = ELASTIC_INSTANCES.get(elastic_key)
