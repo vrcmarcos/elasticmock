3c7600b1f7348e726d85230437b78a8a70d2b1b8
Adam Goldschmidt <adamgold7@gmail.com>
2020-01-28 16:40:12 +0200

    Update fake_elasticsearch.py
    add `doc_type="_doc"` to delete method

--- elasticmock/fake_elasticsearch.py
+++ elasticmock/fake_elasticsearch.py
@@ -227,7 +227,7 @@ class FakeElasticsearch(Elasticsearch):
     
     @query_params('consistency', 'parent', 'refresh', 'replication', 'routing',
                   'timeout', 'version', 'version_type')
-    def delete(self, index, doc_type, id, params=None):
+    def delete(self, index, doc_type="_doc", id, params=None):
 
         found = False
 
