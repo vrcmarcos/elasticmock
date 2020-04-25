d6f58a382120a6d70d5dd63d8e5e9d180b18b789
Christian Gutierrez <chesstrian@gmail.com>
2020-04-25 16:41:08 -0500

    Fix tests for es > 7

--- elasticmock/fake_indices.py
+++ elasticmock/fake_indices.py
@@ -7,23 +7,23 @@ from elasticsearch.client.utils import query_params
 class FakeIndicesClient(IndicesClient):
 
     @query_params('master_timeout', 'timeout')
-    def create(self, index, body=None, params=None):
+    def create(self, index, body=None, params=None, headers=None):
         documents_dict = self.__get_documents_dict()
         if index not in documents_dict:
             documents_dict[index] = []
 
     @query_params('allow_no_indices', 'expand_wildcards', 'ignore_unavailable',
                   'local')
-    def exists(self, index, params=None):
+    def exists(self, index, params=None, headers=None):
         return index in self.__get_documents_dict()
 
     @query_params('allow_no_indices', 'expand_wildcards', 'force',
                   'ignore_unavailable', 'operation_threading')
-    def refresh(self, index=None, params=None):
+    def refresh(self, index=None, params=None, headers=None):
         pass
 
     @query_params('master_timeout', 'timeout')
-    def delete(self, index, params=None):
+    def delete(self, index, params=None, headers=None):
         documents_dict = self.__get_documents_dict()
         if index in documents_dict:
             del documents_dict[index]
