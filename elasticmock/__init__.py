d568fe8f9539dac512d7c514b3f449358dedc9b0
Cody Lee <cody.lee@datadoghq.com>
2021-09-09 13:38:45 -0500

    fix nested context managers and allow for multiple elasticsearch libs to be patched

--- elasticmock/__init__.py
+++ elasticmock/__init__.py
@@ -7,12 +7,25 @@ from six import PY3
 
 if PY3:
     from unittest.mock import patch
+
+    from contextlib import ExitStack, contextmanager
+
+    @contextmanager
+    def nested(*contexts):
+        with ExitStack() as stack:
+            for ctx in contexts:
+                stack.enter_context(ctx)
+            yield contexts
 else:
     from mock import patch
+    from contextlib import nested
+
 
 from elasticmock.fake_elasticsearch import FakeElasticsearch
 
 ELASTIC_INSTANCES = {}
+# if you needed to patch elasticsearch6, elasticsearch7 as well
+ELASTIC_CLASSES_TO_PATCH = {"elasticsearch.Elasticsearch", }
 
 
 def _get_elasticmock(hosts=None, *args, **kwargs):
@@ -33,7 +46,7 @@ def elasticmock(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         ELASTIC_INSTANCES.clear()
-        with patch("elasticsearch.Elasticsearch", _get_elasticmock):
+        with nested(*(patch(klass, _get_elasticmock) for klass in ELASTIC_CLASSES_TO_PATCH)):
             result = f(*args, **kwargs)
         return result
 
